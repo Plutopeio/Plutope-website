6534315015f3a4612898af62bad028c1e7bca9aa
AdityaTeltia <teltia.aditya22@gmail.com>
2022-06-26 14:00:12 +0530

    color fixes
    Signed-off-by: AdityaTeltia <teltia.aditya22@gmail.com>

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -9,7 +9,7 @@ const Landing = () => {
     <Container className='container landing_container'>
       <div className="left_hero">
         <h1>Spend your crypto 
-        <span style={{color: "#844AFF"}}> anywhere in the world </span>
+        <span style={{color: "#3C53F4"}}> anywhere in the world </span>
         with a Plutope debit card.</h1>
         <p className = "landing_para">Say hello👋 to financial freedom.</p>
         <Button variant='contained' href="" className="join_waitlist_btn">Join The Waitlist</Button>
