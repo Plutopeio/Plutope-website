abda3ac09e5da7586addd4c1bbd57315ad4750e9
Ramanjs <raman.s2662@gmail.com>
2022-08-03 13:34:23 +0530

    new footer screen for mobile UI

--- src/components/Footer/FooterMobile.jsx
+++ src/components/Footer/FooterMobile.jsx
@@ -1,25 +1,17 @@
-import React from "react";
 import "./Footer.css";
-import ArrowCircleUpIcon from "@mui/icons-material/ArrowCircleUp";
-import { Link } from "react-scroll";
+import "./FooterMobile.css";
 import Twitter from "../../assets/Twitter.png"
 import Discord from "../../assets/Discord.png"
 import Instagram from "../../assets/Instagram.png"
 import Telegram from "../../assets/Telegram.png"
-import FooterMobile from "./FooterMobile";
-import useMobile from "../../hooks/UseMobile";
-
 import {Button} from "@mui/material";
 
-
-const Footer = () => {
-  const isMobile = useMobile();
-
-  const foot = (
-    <div className="footer_bottom">
-      <div
-        className="footer_bottom_container"
-      >
+const FooterMobile = () => {
+  return (
+    <div
+      className="footer_mobile"
+    >
+      <div className="footer_column">
         <div className="column">
           <div className="header">
             Get Started
@@ -46,7 +38,25 @@ const Footer = () => {
             <a href="">News</a>
             <a href="">Careers</a>
           </div>
-
+          <div className="column">
+            <div className="header">
+              Let's Chat
+            </div>
+            <div className="question">
+              <p style = {{marginBottom: "10px"}}>Have a support question?</p>
+              <Button className = "get_in_touch" variant = "contained" style = {{borderRadius: "78px",background: "#3C53F4" , marginTop : "16px"}}>GET IN TOUCH</Button>
+            </div>
+            <div className="header" style = {{marginTop: "62px"}}>
+              You Call Us
+            </div>
+            <div className="question">
+              <p>0123-456789</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="footer_column">
+        <div className="column">
           <div className="header" style = {{marginTop: "40px"}}>
             Legal
           </div>
@@ -56,7 +66,6 @@ const Footer = () => {
             <a href="">Careers</a>
           </div>
         </div>
-
         <div className="column">
           <div className="header">
             Quick Links
@@ -70,47 +79,9 @@ const Footer = () => {
             <a href = "">Reviews</a>
           </div>
         </div>
-
-        <div className="column">
-          <div className="header">
-            Let's Chat
-          </div>
-          <div className="question">
-            <p style = {{marginBottom: "10px"}}>Have a support question?</p>
-            <Button className = "get_in_touch" variant = "contained" style = {{borderRadius: "78px",background: "#3C53F4" , marginTop : "16px"}}>GET IN TOUCH</Button>
-          </div>
-          <div className="header" style = {{marginTop: "62px"}}>
-            You Call Us
-          </div>
-          <div className="question">
-            <p>0123-456789</p>
-          </div>
-        </div>
       </div>
-    </div>
-  );
-
-  return (
-    <div id = "last" style={{ position: "relative" }}>
-      <Link to="landing" className="scroll_down_arrow to_top" smooth={true}>
-        <ArrowCircleUpIcon style={{ fontSize: "50px", color: "#3C53F4" }} />
-      </Link>
-      <div className="footer">
-        <div
-          className="footer_container"
-        >
-          <h1>
-            Even more exciting features are coming, Join the waitlist and get
-            regular updates.
-          </h1>
-          <button variant="contained" href="" className="join_waitlist_btn">
-            Join The Waitlist
-          </button>
-        </div>
-        {isMobile ? <FooterMobile /> : foot}
-     </div>
-    </div>
-  );
+   </div>
+  )
 };
 
-export default Footer;
+export default FooterMobile;
